73acd695f48c39b3461149982e653750c48bdeef
Erick Rick <erickcredjes@gmail.com>
2023-03-20 10:13:30 -0300

    Configuração inicial
    Eslint, Prettier,  EditorConfig, React Redux e StyledComponents

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 function App() {
-  return <h1>Iniciando projeto</h1>
+  return <h1>2 tarefas pendentes</h1>
 }
 
 export default App
---
+++ src/styles/index.ts
@@ -0,0 +1,11 @@
+import styled, { createGlobalStyle } from 'styled-components'
+
+const EstiloGlobal = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+`
+
+export default EstiloGlobal
